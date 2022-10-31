f348dd98817b6852ccae251724228228a0b5afd8
Teng Zhang <zhangt2333@gmail.com>
2022-10-31 18:00:53 +0800

    feat: write result to html

--- spider.py
+++ spider.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
-# Copyright 2022 zhangt2333. All Rights Reserved.
 import json
+import os
 from datetime import datetime, timedelta
 from time import time
 
+from tabulate import tabulate
 from bs4 import BeautifulSoup
 from requests import Session
 
@@ -12,12 +13,17 @@ from requests import Session
 def extractDigit(s: str) -> int:
     try:
         return int("".join(list(filter(str.isdigit, s))))
-    except Exception as e:
+    except:
         return 0
 
 
-def getGMT8(format: str, deltaMinuites=0) -> str:
+def getGMT8(format="%Y-%m-%d %H:%M", deltaMinuites=0) -> str:
     return (datetime.utcfromtimestamp((time())) + timedelta(hours=8, minutes=deltaMinuites)).strftime(format)
+
+
+def checkAlive(session: Session):
+    resp = session.get("https://api.issks.com/issksapi/V2/ec/chargingList.shtml")
+    return "https://open.weixin.qq.com/connect/oauth2/authorize" not in resp.url
 
 
 session = Session()
@@ -26,25 +32,25 @@ session.headers.update({
     "Host": "api.issks.com"
 })
 session.cookies.update(dict(
-    JSESSIONID="075CE8ED557766E80300AD56B1C88D2E",
+    JSESSIONID=os.getenv("JSESSIONID"),
 ))
 
-# resp = session.get("https://api.issks.com/issksapi/V2/ec/stationList/json.shtml",
-#                    params=dict(mapX=118.958406, mapY=32.119560))
-# assert resp.status_code == 200
-# stations = json.loads(resp.text)["list"]
-# stations = list(filter(lambda s: "南京大学" in s["vStationName"], stations))
+# 验证登录有效
+if not checkAlive(session):
+    print("Cookie失效")
+    exit(-1)
+
 stations = [
-    {"iStationId": 117379, "vStationName": "南京大学仙林校区-第十餐厅1号机"},
-    {"iStationId": 117392, "vStationName": "南京大学仙林校区-第十餐厅2号机"},
-    {"iStationId": 117377, "vStationName": "南京大学仙林校区-游泳馆2号机"},
-    {"iStationId": 117381, "vStationName": "南京大学仙林校区-游泳馆1号机"},
-    # {"iStationId": 117369, "vStationName": "南京大学仙林校区-5栋2号机"},
-    # {"iStationId": 117375, "vStationName": "南京大学仙林校区-5栋1号机"},
-    # {"iStationId": 117387, "vStationName": "南京大学仙林校区-24栋1号机"},
-    # {"iStationId": 117474, "vStationName": "南京大学仙林校区-24栋2号机"},
-    # {"iStationId": 117372, "vStationName": "南京大学仙林校区-17栋2号机"},
-    # {"iStationId": 117374, "vStationName": "南京大学仙林校区-17栋1号机"}
+    {"iStationId": 117379, "vStationName": "第十餐厅1号机"},
+    {"iStationId": 117392, "vStationName": "第十餐厅2号机"},
+    {"iStationId": 117377, "vStationName": "游泳馆2号机"},
+    {"iStationId": 117381, "vStationName": "游泳馆1号机"},
+    # {"iStationId": 117369, "vStationName": "5栋2号机"},
+    # {"iStationId": 117375, "vStationName": "5栋1号机"},
+    # {"iStationId": 117387, "vStationName": "24栋1号机"},
+    # {"iStationId": 117474, "vStationName": "24栋2号机"},
+    # {"iStationId": 117372, "vStationName": "17栋2号机"},
+    # {"iStationId": 117374, "vStationName": "17栋1号机"}
 ]
 
 results = []
@@ -58,16 +64,15 @@ for station in stations:
     for outlet in outlets:
         resp = session.get(f"https://api.issks.com/issksapi/V2/ec/charging/{outlet['vOutletNo']}.shtml")
         assert resp.status_code == 200
+        isUsing, power, usedTime, totalTime = False, 0, 0, 0
         soup = BeautifulSoup(resp.text, "lxml")
-        isUsing = soup.select_one(".state_item") is not None
-        if isUsing:
+        if soup.select_one(".state_item") is not None:
+            isUsing = True
             power = extractDigit(soup.select_one(".state_item:nth-child(1) p").text)  # 瓦
             usedTime = extractDigit(soup.select_one(".state_item:nth-child(2) p").text)  # 分钟
             totalTime = extractDigit(soup.select(".state_item:nth-child(2) span")[2].text) * 30  # 分钟
             if totalTime == 0:
                 totalTime = 999
-        else:
-            power, usedTime, totalTime = 0, 0, 0
         results.append(dict(
             vStationName=station["vStationName"],
             vOutletName=outlet["vOutletName"],
@@ -76,12 +81,35 @@ for station in stations:
             usedTime=usedTime,
             totalTime=totalTime,
             remainingTime=totalTime - usedTime,
-            desc=f"充电{usedTime}/{totalTime}分钟, 还剩{totalTime - usedTime}分钟, 即{(totalTime - usedTime) // 60}小时{(totalTime - usedTime) % 60}分",
-            endTime=getGMT8('%Y-%m-%d %H:%M', totalTime - usedTime)
+            usedAndTotalTimeDesc=f"{usedTime}/{totalTime}分钟",
+            remainingTimeDesc=f"{(totalTime - usedTime) // 60}小时{(totalTime - usedTime) % 60}分",
+            endTime=getGMT8("%Y-%m-%d %H:%M", totalTime - usedTime)
         ))
 
-for result in sorted(results, key=lambda x: x["remainingTime"]):
+results = sorted(results, key=lambda x: x["remainingTime"])
+
+tableHeader = ("充电桩", "插座号", "充电时长", "剩余时长", "结束时间")
+tableBody = []
+for result in results:
     if result["isUsing"]:
-        print(result["vStationName"], result["vOutletName"], result["desc"], result["endTime"])
+        tableBody.append([result["vStationName"],
+                          result["vOutletName"],
+                          result["usedAndTotalTimeDesc"],
+                          result["remainingTimeDesc"],
+                          result["endTime"]])
     else:
-        print(result["vStationName"], result["vOutletName"])
+        tableBody.append([result["vStationName"],
+                          result["vOutletName"],
+                          "", "", ""])
+html = """
+<style>
+table, th, td {
+  border: 1px solid;
+}
+</style>
+""" + f"""
+<h3>更新时间：{getGMT8()}</h3>
+{tabulate(tableBody, tableHeader, tablefmt="html")}
+"""
+
+open("index.html", mode="w", encoding="utf-8").write(html)
