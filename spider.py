ddb8c94a2b7646560f46514c130bd1eb87a1e1be
Teng Zhang <zhangt2333@gmail.com>
2023-02-21 17:05:29 +0800

    feat: showing statistic in separate areas

--- spider.py
+++ spider.py
@@ -4,10 +4,37 @@ import json
 import os
 from datetime import datetime, timedelta
 from time import time
+from typing import List
 
 from tabulate import tabulate
 from bs4 import BeautifulSoup
 from requests import Session
+
+
+class ChargingStation:
+    def __init__(self,
+                 stationName: str,
+                 outletName: str,
+                 area: str,
+                 isUsing: bool,
+                 power: int,
+                 usedTime: int,
+                 totalTime: int):
+        self.stationName = stationName
+        self.outletName = outletName
+        self.area = area
+        self.isUsing = isUsing
+        self.power = power
+        self.usedTime = usedTime
+        self.totalTime = totalTime
+        self.remainingTime = totalTime - usedTime
+        self.usedAndTotalTimeDesc = ""
+        self.remainingTimeDesc = ""
+        self.endTimeDesc = ""
+        if isUsing:
+            self.usedAndTotalTimeDesc = f"{usedTime}/{totalTime}分钟"
+            self.remainingTimeDesc = f"{self.remainingTime // 60}小时{self.remainingTime % 60}分钟"
+            self.endTimeDesc = getGMT8("%Y-%m-%d %H:%M", self.remainingTime)
 
 
 def extractDigit(s: str) -> int:
@@ -17,8 +44,8 @@ def extractDigit(s: str) -> int:
         return 0
 
 
-def getGMT8(format="%Y-%m-%d %H:%M", deltaMinuites=0) -> str:
-    return (datetime.utcfromtimestamp((time())) + timedelta(hours=8, minutes=deltaMinuites)).strftime(format)
+def getGMT8(format="%Y-%m-%d %H:%M", deltaMinutes=0) -> str:
+    return (datetime.utcfromtimestamp((time())) + timedelta(hours=8, minutes=deltaMinutes)).strftime(format)
 
 
 def checkAlive(session: Session):
@@ -40,22 +67,20 @@ if not checkAlive(session):
     print("Cookie失效")
     exit(-1)
 
-stations = [
-    {"iStationId": 117379, "vStationName": "第十餐厅1号机"},
-    {"iStationId": 117392, "vStationName": "第十餐厅2号机"},
-    {"iStationId": 117377, "vStationName": "游泳馆2号机"},
-    {"iStationId": 117381, "vStationName": "游泳馆1号机"},
-    # {"iStationId": 117369, "vStationName": "5栋2号机"},
-    # {"iStationId": 117375, "vStationName": "5栋1号机"},
-    {"iStationId": 117387, "vStationName": "24栋1号机"},
-    {"iStationId": 117474, "vStationName": "24栋2号机"},
-    {"iStationId": 117372, "vStationName": "17栋2号机"},
-    {"iStationId": 117374, "vStationName": "17栋1号机"},
-]
+results: List[ChargingStation] = []
 
-results = []
-
-for station in stations:
+for station in [
+    {"iStationId": 117379, "vStationName": "第十餐厅1号机", "area": "三组团"},
+    {"iStationId": 117392, "vStationName": "第十餐厅2号机", "area": "三组团"},
+    {"iStationId": 117387, "vStationName": "24栋1号机", "area": "四组团"},
+    {"iStationId": 117474, "vStationName": "24栋2号机", "area": "四组团"},
+    {"iStationId": 117372, "vStationName": "17栋2号机", "area": "四组团"},
+    {"iStationId": 117374, "vStationName": "17栋1号机", "area": "四组团"},
+    {"iStationId": 117377, "vStationName": "游泳馆2号机", "area": "二组团"},
+    {"iStationId": 117381, "vStationName": "游泳馆1号机", "area": "二组团"},
+    # {"iStationId": 117369, "vStationName": "5栋2号机", "area": "一组团"},
+    # {"iStationId": 117375, "vStationName": "5栋1号机", "area": "一组团"},
+]:
     stationId = station["iStationId"]
     resp = session.get("https://api.issks.com/issksapi/V2/ec/chargingList.shtml",
                        params=dict(stationId=stationId))
@@ -64,43 +89,20 @@ for station in stations:
     for outlet in outlets:
         resp = session.get(f"https://api.issks.com/issksapi/V2/ec/charging/{outlet['vOutletNo']}.shtml")
         assert resp.status_code == 200
+
         isUsing, power, usedTime, totalTime = False, 0, 0, 0
-        soup = BeautifulSoup(resp.text, "lxml")
-        if soup.select_one(".state_item") is not None:
+        if (soup := BeautifulSoup(resp.text, "lxml")).select_one(".state_item"):
             isUsing = True
             power = extractDigit(soup.select_one(".state_item:nth-child(1) p").text)  # 瓦
             usedTime = extractDigit(soup.select_one(".state_item:nth-child(2) p").text)  # 分钟
             totalTime = extractDigit(soup.select(".state_item:nth-child(2) span")[2].text) * 30  # 分钟
-            if totalTime == 0:
-                totalTime = 999
-        results.append(dict(
-            vStationName=station["vStationName"],
-            vOutletName=outlet["vOutletName"],
-            isUsing=isUsing,
-            power=power,
-            usedTime=usedTime,
-            totalTime=totalTime,
-            remainingTime=totalTime - usedTime,
-            usedAndTotalTimeDesc=f"{usedTime}/{totalTime}分钟",
-            remainingTimeDesc=f"{(totalTime - usedTime) // 60}小时{(totalTime - usedTime) % 60}分",
-            endTime=getGMT8("%Y-%m-%d %H:%M", totalTime - usedTime)
-        ))
+            totalTime = totalTime if totalTime != 0 else 999
 
-results = sorted(results, key=lambda x: x["remainingTime"])
+        results.append(ChargingStation(station["vStationName"], outlet["vOutletName"], station["area"],
+                                       isUsing, power, usedTime, totalTime))
 
-tableHeader = ("充电桩", "插座号", "充电时长", "剩余时长", "结束时间")
-tableBody = []
-for result in results:
-    if result["isUsing"]:
-        tableBody.append([result["vStationName"],
-                          result["vOutletName"],
-                          result["usedAndTotalTimeDesc"],
-                          result["remainingTimeDesc"],
-                          result["endTime"]])
-    else:
-        tableBody.append([result["vStationName"],
-                          result["vOutletName"],
-                          "", "", ""])
+results = sorted(results, key=lambda x: x.remainingTime)
+
 html = """
 <style>
 table, th, td {
@@ -108,9 +110,18 @@ table, th, td {
 }
 </style>
 """ + f"""
-<h3>更新时间：{getGMT8()}</h3>
-{tabulate(tableBody, tableHeader, tablefmt="html")}
+<h2>更新时间：{getGMT8()}</h2>
 """
-if not os.path.exists("public"):
-    os.mkdir("public")
+htmlTableHeader = ("充电桩", "插座号", "充电时长", "剩余时长", "结束时间")
+
+areas = ["三组团", "四组团", "二组团", "一组团"]
+for area in areas:
+    html += f"<h4>{area}</h4>"
+    html += tabulate(list(map(lambda x: [x.stationName, x.outletName, x.usedAndTotalTimeDesc,
+                                         x.remainingTimeDesc, x.endTimeDesc],
+                              filter(lambda x: x.area == area, results))),
+                     htmlTableHeader, tablefmt="html")
+
+if not os.path.exists("build"):
+    os.mkdir("build")
 open("public/index.html", mode="w", encoding="utf-8").write(html)
