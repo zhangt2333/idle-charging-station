9786daf15021c98aba9d12a9253e7b6f2aeda4de
Teng Zhang <zhangt2333@gmail.com>
2023-07-28 23:10:49 +0800

    refactor: use coroutine to speed up the spider

--- spider.py
+++ spider.py
@@ -5,11 +5,40 @@ from dataclasses import dataclass
 from enum import Enum
 from collections import OrderedDict
 
+import asyncio
+import aiohttp
 from tabulate import tabulate
 from bs4 import BeautifulSoup
-from requests import Session
+from aiohttp import ClientSession
 
 MAX_MINUTES = 999
+
+stations = [
+    {"iStationId": 158789, "vStationName": "26栋1号机", "area": "四组团-26栋"},
+    {"iStationId": 158742, "vStationName": "26栋2号机", "area": "四组团-26栋"},
+    {"iStationId": 158664, "vStationName": "26栋3号机", "area": "四组团-26栋"},
+    {"iStationId": 158507, "vStationName": "27栋1号机", "area": "四组团-27栋"},
+    {"iStationId": 158748, "vStationName": "27栋2号机", "area": "四组团-27栋"},
+    {"iStationId": 158501, "vStationName": "27栋3号机", "area": "四组团-27栋"},
+    {"iStationId": 158764, "vStationName": "27栋4号机", "area": "四组团-27栋"},
+    {"iStationId": 158666, "vStationName": "27栋5号机", "area": "四组团-27栋"},
+    {"iStationId": 158661, "vStationName": "27栋6号机", "area": "四组团-27栋"},
+    {"iStationId": 158668, "vStationName": "27栋7号机", "area": "四组团-27栋"},
+    {"iStationId": 117387, "vStationName": "24栋1号机", "area": "四组团-24栋"},
+    {"iStationId": 117474, "vStationName": "24栋2号机", "area": "四组团-24栋"},
+    {"iStationId": 117374, "vStationName": "17栋（8插座）1号机", "area": "四组团-17栋"},
+    {"iStationId": 117372, "vStationName": "17栋（8插座）2号机", "area": "四组团-17栋"},
+    {"iStationId": 165088, "vStationName": "17栋（8插座）3号机", "area": "四组团-17栋"},
+    {"iStationId": 158676, "vStationName": "17栋（10插座）1号机", "area": "四组团-17栋"},
+    {"iStationId": 157805, "vStationName": "17栋（10插座）2号机", "area": "四组团-17栋"},
+    {"iStationId": 157551, "vStationName": "17栋（10插座）3号机", "area": "四组团-17栋"},
+    # {"iStationId": 117379, "vStationName": "第十餐厅1号机", "area": "三组团"},
+    # {"iStationId": 117392, "vStationName": "第十餐厅2号机", "area": "三组团"},
+    # {"iStationId": 117377, "vStationName": "游泳馆2号机", "area": "二组团"},
+    # {"iStationId": 117381, "vStationName": "游泳馆1号机", "area": "二组团"},
+    # {"iStationId": 117369, "vStationName": "5栋2号机", "area": "一组团"},
+    # {"iStationId": 117375, "vStationName": "5栋1号机", "area": "一组团"},
+]
 
 
 @dataclass
@@ -67,95 +96,86 @@ def getDatetime(fmt="%Y-%m-%d %H:%M:%S", timezone=8, deltaMinutes=0):
             .strftime(fmt))
 
 
-def checkAlive(session: Session):
-    resp = session.get("https://api.issks.com/issksapi/V2/ec/chargingList.shtml")
-    return "https://open.weixin.qq.com/connect/oauth2/authorize" not in resp.url
+async def checkAlive(session: ClientSession) -> bool:
+    async with session.get("https://api.issks.com/issksapi/V2/ec/userInfo.shtml") as resp:
+        text = await resp.text()
+        return text.strip() != ""
 
 
-session = Session()
-session.headers.update({
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307061d)",
-    "Host": "api.issks.com"
-})
-session.cookies.update(dict(
-    JSESSIONID=os.getenv("JSESSIONID"),
-))
+async def fetchOutletList(session: ClientSession, station: dict) -> list[dict]:
+    async with session.get("https://api.issks.com/issksapi/V2/ec/chargingList.shtml",
+                           params=dict(stationId=station["iStationId"])) as resp:
+        assert resp.status == 200
+        return (await resp.json())["list"]
 
-# 验证登录有效
-if not checkAlive(session):
-    print("Cookie失效")
-    exit(-1)
 
-results: list[ChargingStation] = []
-
-stations = [
-    {"iStationId": 158789, "vStationName": "26栋1号机", "area": "四组团-26栋"},
-    {"iStationId": 158742, "vStationName": "26栋2号机", "area": "四组团-26栋"},
-    {"iStationId": 158664, "vStationName": "26栋3号机", "area": "四组团-26栋"},
-    {"iStationId": 158507, "vStationName": "27栋1号机", "area": "四组团-27栋"},
-    {"iStationId": 158748, "vStationName": "27栋2号机", "area": "四组团-27栋"},
-    {"iStationId": 158501, "vStationName": "27栋3号机", "area": "四组团-27栋"},
-    {"iStationId": 158764, "vStationName": "27栋4号机", "area": "四组团-27栋"},
-    {"iStationId": 158666, "vStationName": "27栋5号机", "area": "四组团-27栋"},
-    {"iStationId": 158661, "vStationName": "27栋6号机", "area": "四组团-27栋"},
-    {"iStationId": 158668, "vStationName": "27栋7号机", "area": "四组团-27栋"},
-    {"iStationId": 117387, "vStationName": "24栋1号机", "area": "四组团-24栋"},
-    {"iStationId": 117474, "vStationName": "24栋2号机", "area": "四组团-24栋"},
-    {"iStationId": 117374, "vStationName": "17栋（8插座）1号机", "area": "四组团-17栋"},
-    {"iStationId": 117372, "vStationName": "17栋（8插座）2号机", "area": "四组团-17栋"},
-    {"iStationId": 165088, "vStationName": "17栋（8插座）3号机", "area": "四组团-17栋"},
-    {"iStationId": 158676, "vStationName": "17栋（10插座）1号机", "area": "四组团-17栋"},
-    {"iStationId": 157805, "vStationName": "17栋（10插座）2号机", "area": "四组团-17栋"},
-    {"iStationId": 157551, "vStationName": "17栋（10插座）3号机", "area": "四组团-17栋"},
-    # {"iStationId": 117379, "vStationName": "第十餐厅1号机", "area": "三组团"},
-    # {"iStationId": 117392, "vStationName": "第十餐厅2号机", "area": "三组团"},
-    # {"iStationId": 117377, "vStationName": "游泳馆2号机", "area": "二组团"},
-    # {"iStationId": 117381, "vStationName": "游泳馆1号机", "area": "二组团"},
-    # {"iStationId": 117369, "vStationName": "5栋2号机", "area": "一组团"},
-    # {"iStationId": 117375, "vStationName": "5栋1号机", "area": "一组团"},
-]
-
-for station in stations:
-    stationId = station["iStationId"]
-    resp = session.get("https://api.issks.com/issksapi/V2/ec/chargingList.shtml",
-                       params=dict(stationId=stationId))
-    assert resp.status_code == 200
-    outlets = resp.json()["list"]
-    for outlet in outlets:
-        resp = session.get(f"https://api.issks.com/issksapi/V2/ec/charging/{outlet['vOutletNo']}.shtml")
-        assert resp.status_code == 200
+async def fetchOutletDetail(session: ClientSession, station: dict, outlet: dict) -> ChargingStation:
+    async with session.get(f"https://api.issks.com/issksapi/V2/ec/charging/{outlet['vOutletNo']}.shtml") as resp:
+        assert resp.status == 200
+        text = await resp.text()
         chargingStation = ChargingStation(station["vStationName"], outlet["vOutletName"], station["area"])
-        if "设备维护中" in resp.text or outlet["status"] == 3:  # status=3 时页面提示该插座安全隐患不可用
+        if "设备维护中" in text or outlet["status"] == 3:  # status=3 时页面提示该插座安全隐患不可用
             chargingStation.status = ChargingStation.Status.UNAVAILABLE
-            continue
-        elif (soup := BeautifulSoup(resp.text, "lxml")).select_one(".state_item"):
+        elif (soup := BeautifulSoup(text, "lxml")).select_one(".state_item"):
             chargingStation.status = ChargingStation.Status.USING
             chargingStation.power = extractDigit(soup.select_one(".state_item:nth-child(1) p").text)  # 瓦
             chargingStation.usedMinutes = extractDigit(soup.select_one(".state_item:nth-child(2) p").text)  # 分钟
             totalMinutes = extractDigit(soup.select(".state_item:nth-child(1) span")[-1].text) * 60  # 分钟
             chargingStation.totalMinutes = totalMinutes if totalMinutes != 0 else 600
-        results.append(chargingStation)
+        return chargingStation
 
-results = sorted(results, key=lambda x: x.remainingMinutes)
 
-html = """
-<style>
-table, th, td {
-  border: 1px solid;
-}
-</style>
-""" + f"""
-<h1>更新时间：{getDatetime(fmt="%Y-%m-%d %H:%M")}</h1>
-"""
-htmlTableHeader = ("充电桩", "插座号", "充电时长", "剩余时长", "结束时间", "备注")
+async def main():
+    session = aiohttp.ClientSession(trust_env=True)
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6307061d)",
+        "Host": "api.issks.com"
+    })
+    session.cookie_jar.update_cookies(dict(
+        JSESSIONID=os.getenv("JSESSIONID"),
+    ))
 
-for area in list(OrderedDict.fromkeys([s["area"] for s in stations])):
-    html += f"<h3>{area}</h3>"
-    html += tabulate(list(map(lambda x: [x.stationName, x.outletName, x.usedAndTotalMinutesDesc,
-                                         x.remainingTimeDesc, x.endTimeDesc, x.note],
-                              filter(lambda x: x.area == area, results))),
-                     htmlTableHeader, tablefmt="html")
+    # 验证登录有效
+    if not await checkAlive(session):
+        print("Cookie失效")
+        exit(-1)
 
-if not os.path.exists("build"):
-    os.mkdir("build")
-open("build/index.html", mode="w", encoding="utf-8").write(html)
+    tasks = []
+    for station in stations:
+        tasks.append(asyncio.ensure_future(fetchOutletList(session, station)))
+    outletLists = await asyncio.gather(*tasks)
+
+    tasks = []
+    for station, outlets in zip(stations, outletLists):
+        for outlet in outlets:
+            tasks.append(asyncio.ensure_future(fetchOutletDetail(session, station, outlet)))
+    results = await asyncio.gather(*tasks)
+
+    await session.close()
+
+    results = sorted(results, key=lambda x: x.remainingMinutes)
+
+    html = """
+    <style>
+    table, th, td {
+      border: 1px solid;
+    }
+    </style>
+    """ + f"""
+    <h1>更新时间：{getDatetime(fmt="%Y-%m-%d %H:%M")}</h1>
+    """
+    htmlTableHeader = ("充电桩", "插座号", "充电时长", "剩余时长", "结束时间", "备注")
+
+    for area in list(OrderedDict.fromkeys([s["area"] for s in stations])):
+        html += f"<h3>{area}</h3>"
+        html += tabulate(list(map(lambda x: [x.stationName, x.outletName, x.usedAndTotalMinutesDesc,
+                                             x.remainingTimeDesc, x.endTimeDesc, x.note],
+                                  filter(lambda x: x.area == area, results))),
+                         htmlTableHeader, tablefmt="html")
+
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    open("build/index.html", mode="w", encoding="utf-8").write(html)
+
+
+asyncio.run(main())
